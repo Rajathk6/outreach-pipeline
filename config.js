99b98df4be68aab01f98be901ff5bc5e305d18a7
rajath <rajathkumar120@gmail.com>
2026-06-07 19:02:26 +0530

    fix: added comments and info

--- config.js
+++ config.js
@@ -14,6 +14,10 @@ module.exports = {
     apiKey: process.env.BREVO_API_KEY,
     baseUrl: 'https://api.brevo.com/v3'
   },
+  companyEnrich: {
+    apiKey: process.env.COMPANY_ENRICH_API_KEY,
+    baseUrl: 'https://api.companyenrich.com'
+  },
   sender: {
     email: process.env.SENDER_EMAIL,
     name: process.env.SENDER_NAME
